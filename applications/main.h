0a5df6bf4ba7697c091b1ba0c2c5c8462d262a6a
禹云飞 <yuyf@audfly2020.com>
2025-01-10 16:36:02 +0800

    First commit to origin

---
+++ applications/main.h
@@ -0,0 +1,33 @@
+#ifndef __main_H_ //shift+U转换为大写
+#define __main_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "bsp.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
